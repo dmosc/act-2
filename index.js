56dd7eadbdcc319eb4ae3e0f8d7913d5c5bd7c64
oscardavidrm <oscardavidrodriguezm@gmail.com>
2022-03-15 20:07:23 -0600

    Vue test app.

---
+++ index.js
@@ -0,0 +1,12 @@
+Vue.createApp({
+  data() {
+    return {
+      information: {
+        name: "Oscar D. Rodriguez Moscosa",
+        id: "A01229551"
+      },
+      interests: ["Watching movies", "Exercising", "Web3"],
+      picsumImage: "https://picsum.photos/200/300"
+    };
+  }
+}).mount("#app");
